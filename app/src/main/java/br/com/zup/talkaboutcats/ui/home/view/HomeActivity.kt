e89d603616bd8565a01ca8b63facab9cb576404f
ChristalCamillo <christal.b.camillo@gmail.com>
2022-12-16 16:32:46 -0300

    navigation component finished

---
+++ app/src/main/java/br/com/zup/talkaboutcats/ui/home/view/HomeActivity.kt
@@ -0,0 +1,10 @@
+package br.com.zup.talkaboutcats.ui.home.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
