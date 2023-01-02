bb65f485b86b3269e07004f3f57a2aa2ac0f700d
ChristalCamillo <christal.b.camillo@gmail.com>
2023-01-02 12:01:59 -0300

    manifest updated, firebase added, constants for login and register added.

---
+++ app/src/main/java/br/com/zup/talkaboutcats/ui/splash/view/SplashActivity.kt
@@ -0,0 +1,10 @@
+package br.com.zup.talkaboutcats.ui.splash.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/br/com/zup/talkaboutcats/utils/Constants.kt
@@ -0,0 +1,14 @@
+package br.com.zup.talkaboutcats.utils
+
+const val NAME_ERROR_MESSAGE = "Favor informar nome"
+const val EMAIL_ERROR_MESSAGE = "Favor informar email"
+const val PASSWORD_ERROR_MESSAGE = "Favor informar senha"
+const val REGISTER_ERROR_MESSAGE = "Não foi possivel realizar o registro"
+const val LOGIN_ERROR_MESSAGE = "Não foi possivel realizar o login!"
+const val USER_KEY = "USER"
+const val ERROR_VALIDATE_NAME = "Seu nome deve ter no mínimo 3 caracteres"
+const val ERROR_VALIDATE_PASSWORD = "Sua senha deve conter no mínimo 8 caracteres"
+const val INVALID_EMAIL = "E-mail inválido"
+const val REGISTER = "Registro"
+const val LOGIN = "Login"
+const val EMAIL_REGEX= "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"
